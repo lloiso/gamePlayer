20728dfb7abdcf57a2a6e2b878b0c26a106f02e4
Dmitrii <lisp.tnt@gmail.com>
2022-04-03 12:42:43 +0300

    final

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,27 @@
-public class Player {
+public class Player extends Human{
+
+    class Backpack{
+        private String staff = "";
+        void put(String thing){
+            staff += thing +",";
+        }
+        public String toString(){
+            if (staff.equals("")) {
+                return "the backpack is empty";
+            }
+            return staff.substring(0, staff.length()-1) + " in the backpack";
+        }
+    }
+
+    Backpack backpack;
+
+    public Player(String name) {
+        super(name + " the Player");
+        backpack = new Backpack();
+    }
+
+    public void take(String thing){
+        backpack.put(thing);
+    }
 
 }
